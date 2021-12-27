b7eae1f1ca6ca4f62deaa56d6e87ddea9bb4dbb1
Mariia Bila <freiheit.maria@gmail.com>
2021-12-27 10:34:09 +0200

    draft

--- src/App.js
+++ src/App.js
@@ -1,23 +1,85 @@
-import logo from './logo.svg';
-import './App.css';
+import React, {useState} from 'react';
+import './App.scss';
+import { ContactDetails } from './ContactDetails/ContactDetails';
+import ContactsList from './ContactsList/ContactsList';
+import NewContactForm from './NewContactForm/NewContactForm';
+
+
+const contact0 = {
+  id: 0,
+  contactName: '',
+  surname:'',
+  email:'',
+  tel: '',
+  dateOfBirth: '',
+}
 
 function App() {
+  const [contacts, setContacts] = useState([]);
+  const [detailsIsShown, setDetailsIsShown] = useState(false);
+  const [formIsShown, setFormIsShown] = useState(false);
+  const [selectedContact, setSelectedContact] = useState(contact0); /*should work with null */
+
+  const addContact = (contact) => {
+    setContacts([contact, ...contacts])
+    // localStorage.setItem(contact.id, JSON.stringify(contacts))
+  };
+
+  const deleteContact = (contact) => {
+    setContacts(contacts.filter(el => el.id !== contact.id))
+    // localStorage.removeItem(contact.id, JSON.stringify(contacts))
+  };
+
+  const editContact = (currentContact, changedContact) => {
+    setContacts(contacts.map(el => {
+     if ( el.id !== currentContact.id) {
+       return el;
+     };
+    //  setSelectedContact(contact2)
+     return changedContact;
+    }))
+    // localStorage.removeItem(contact.id, JSON.stringify(contacts))
+  };
+
+  const toggleDetailsPage = () => {
+    setDetailsIsShown(!detailsIsShown)
+  };
+
+  const selectContact = (contact) => {
+    setSelectedContact(contact)
+  };
+
+  const hideForm = () => {
+    setFormIsShown(false)
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+      <h1 className="title">My contact list</h1>
+      {!detailsIsShown && (
+        <ContactsList
+          contacts={contacts}
+          deleteContact={deleteContact}
+          toggleDetailsPage={toggleDetailsPage}
+          selectContact={selectContact}
+        />
+      )}
+      
+      {detailsIsShown && (
+        <ContactDetails
+          selectedContact={selectedContact}
+          toggleDetailsPage={toggleDetailsPage} 
+          editContact={editContact}
+        />)}
+
+      {formIsShown && (<NewContactForm addContact={addContact} hideForm={hideForm} />)}
+
+      <button
+          className="button"
+          onClick={()=> setFormIsShown(!formIsShown)}
         >
-          Learn React
-        </a>
-      </header>
+          {!formIsShown ? ('+  Add contact') : ('Cancel')}
+        </button>
     </div>
   );
 }
